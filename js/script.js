c15edd89f390a0300aef7d3a4360302b0fd95066
Alexander <sanexwell@bk.ru>
2020-11-10 11:03:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.header__burger').click(function() {
+        $('.header__nav').toggleClass('header__active')
+    });
+    $('.header__select').click(function() {
+        $('.header__nav').toggleClass('header__active')
+    });
+    $('.header__burger').click(function() {
+        $('.header__burger').toggleClass('header__active')
+    });
+    $('.header__select').click(function() {
+        $('.header__burger').toggleClass('header__active')
+    });
+});
